273c05f7107331d0e215b192f13af43a1efc9063
Zara-Gma <zara.gma@gmail.com>
2021-01-31 22:59:16 -0800

    message

--- src/memberForm/2Identity.js
+++ src/memberForm/2Identity.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import { useHistory } from "react-router-dom";
-import { useData } from "../DataContext";
-import { Controller, useForm } from "react-hook-form";
-import { Form } from "../components/Form";
-import {
-  Checkbox,
-  Grid,
-  InputLabel,
-  TextField
-} from "@material-ui/core";
-// import { yupResolver } from "@hookform/resolvers";
-// import * as yup from "yup";
-import ReactSelect from "react-select";
-import Veteran from "../constants/veteranSelectOptions";
+// import React from 'react';
+// import { useHistory } from "react-router-dom";
+// import { useData } from "../DataContext";
+// import { Controller, useForm } from "react-hook-form";
+// import { Form } from "../components/Form";
+// import {
+//   Checkbox,
+//   Grid,
+//   InputLabel,
+//   TextField
+// } from "@material-ui/core";
+// // import { yupResolver } from "@hookform/resolvers";
+// // import * as yup from "yup";
+// import ReactSelect from "react-select";
+// import Veteran from "../constants/veteranSelectOptions";
 
-// const schema = yup.object().shape({
-//   veteranStatus: yup
-//     .string(),
-//   identify: yup
-//     .string(),
-//   comment: yup.string()
-//     .min(10, "Must be at least 3 characters")
-// })
+// // const schema = yup.object().shape({
+// //   veteranStatus: yup
+// //     .string(),
+// //   identify: yup
+// //     .string(),
+// //   comment: yup.string()
+// //     .min(10, "Must be at least 3 characters")
+// // })
 
 
-export const Identity = () => {
-  const { setValues, data } = useData();
-  const history = useHistory();
-  const { register, handleSubmit, control } = useForm({
-    defaultValues: {
-      veteranStatus: data.veteranStatus,
-      identify: data.identify,
-      comments: data.comments
-    },
-    mode: "onBlur",
-    // resolver: yupResolver(schema),
-  });
-  const onSubmit = () => {
-    history.push("./interests");
-    setValues(data);
-  };
+// export const Identity = () => {
+//   const { setValues, data } = useData();
+//   const history = useHistory();
+//   const { register, handleSubmit, control } = useForm({
+//     defaultValues: {
+//       veteranStatus: data.veteranStatus,
+//       identify: data.identify,
+//       comments: data.comments
+//     },
+//     mode: "onBlur",
+//     // resolver: yupResolver(schema),
+//   });
+//   const onSubmit = () => {
+//     history.push("./interests");
+//     setValues(data);
+//   };
 
-  return (
-    <React.Fragment>
-      <Form>
-      </Form>
-    </React.Fragment >
-  );
-};
+//   return (
+//     <React.Fragment>
+//       <Form>
+//       </Form>
+//     </React.Fragment >
+//   );
+// };
